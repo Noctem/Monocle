003e025a583bbae986e9e9bd4831a2fb2f8ae1e5
David Christenson <david@the-david.com>
2016-08-09 04:16:40 -0600

    Define range with tuple and restore compatibility
    Use tuple to define SCAN_DELAY range instead of two variables, retain
    backwards compatibility with SCAN_DELAY as an int.

--- worker.py
+++ worker.py
@@ -221,9 +221,12 @@ class Slave(threading.Thread):
             if self.error_code and self.seen_per_cycle:
                 self.error_code = None
             self.step += 1
-            time.sleep(
-                random.uniform(config.SCAN_DELAY, config.SCAN_DELAY_HIGH)
-            )
+            if isinstance(config.SCAN_DELAY, tuple):
+                time.sleep(random.uniform(*config.SCAN_DELAY))
+            else:
+                time.sleep(
+                    random.uniform(config.SCAN_DELAY, config.SCAN_DELAY + 2)
+                )
         session.close()
         if self.seen_per_cycle == 0:
             self.error_code = 'NO POKEMON'
