53d80dd302d89cb7229420eab4c0fe7037a8db36
Modrzew <dev@modriv.net>
2016-08-15 17:23:01 +0200

    Make sure that what comes from backend is a dict
    Otherwise - restart worker.
    And a bit of logging by the way!

--- worker.py
+++ worker.py
@@ -40,8 +40,8 @@ workers = {}
 local_data = threading.local()
 
 
-class CannotProcessStep(Exception):
-    """Raised when servers are too busy"""
+class MalformedResponse(Exception):
+    """Raised when server response is malformed"""
 
 
 def configure_logger(filename='worker.log'):
@@ -107,18 +107,22 @@ class Slave(threading.Thread):
                     self.restart()
                     return
             except pgoapi_exceptions.AuthException:
+                self.logger.warning('Login failed!')
                 self.error_code = 'LOGIN FAIL'
                 self.restart()
                 return
             except pgoapi_exceptions.NotLoggedInException:
+                self.logger.error('Invalid credentials')
                 self.error_code = 'BAD LOGIN'
                 self.restart()
                 return
             except pgoapi_exceptions.ServerBusyOrOfflineException:
+                self.logger.info('Server too busy - restarting')
                 self.error_code = 'RETRYING'
                 self.restart()
                 return
             except pgoapi_exceptions.ServerSideRequestThrottlingException:
+                self.logger.info('Server throttling - sleeping for a bit')
                 time.sleep(random.uniform(1, 5))
                 continue
             except Exception:
@@ -133,7 +137,8 @@ class Slave(threading.Thread):
                 return
             try:
                 self.main()
-            except CannotProcessStep:
+            except MalformedResponse:
+                self.logger.warning('Malformed response received!')
                 self.error_code = 'RESTART'
                 self.restart()
             except Exception:
@@ -146,9 +151,11 @@ class Slave(threading.Thread):
                 return
             self.cycle += 1
             if self.cycle <= config.CYCLES_PER_WORKER:
+                self.logger.info('Going to sleep for a bit')
                 self.error_code = 'SLEEP'
                 self.running = False
                 time.sleep(random.randint(30, 60))
+                self.logger.info('AWAKEN MY MASTERS')
                 self.running = True
                 self.error_code = None
         self.error_code = 'RESTART'
@@ -171,9 +178,13 @@ class Slave(threading.Thread):
                 longitude=pgoapi_utils.f2i(point[1]),
                 cell_id=cell_ids
             )
-            logger.debug('Response: %s', response_dict)
-            if response_dict is False:
-                raise CannotProcessStep
+            if not isinstance(response_dict, dict):
+                logger.warning('Response: %s', response_dict)
+                raise MalformedResponse
+            responses = response_dict.get('responses')
+            if not responses:
+                logger.warning('Response: %s', response_dict)
+                raise MalformedResponse
             map_objects = response_dict['responses'].get('GET_MAP_OBJECTS', {})
             pokemons = []
             forts = []
