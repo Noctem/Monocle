e058e4783b5e03853e8b07c7fe2f33dda365dee1
David Christenson <david@the-david.com>
2016-11-18 20:26:15 -0700

    Improve CAPTCHA solving script
    Use MAP_START and MAP_END instead of hardcoded values for location,
    use one Chrome instance instead of launching one per CAPTCHA, only
    request altitude once and randomize it slightly, handle exceptions
    and KeyboardInterrupts.

--- solve_captchas.py
+++ solve_captchas.py
@@ -13,6 +13,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from config import MAP_START, MAP_END
+from sys import exit
 
 def get_device_info(account):
     hardware = {'iPhone5,1': 'N41AP',
@@ -37,41 +39,59 @@ def get_device_info(account):
     device_info['device_id'] = account[5]
     return device_info
 
-def resolve_captcha(url, api):
-    driver = webdriver.Chrome()
-    driver.set_window_size(803, 807)
-    driver.get(url)
-    WebDriverWait(driver, 86400).until(EC.text_to_be_present_in_element_value((By.NAME, "g-recaptcha-response"), ""))
-    driver.switch_to.frame(driver.find_element_by_xpath("//*/iframe[@title='recaptcha challenge']"))
-    token = driver.find_element_by_id("recaptcha-token").get_attribute("value")
-    driver.close()
-    response = api.verify_challenge(token=token)
-    success = response.get('responses', {}).get('VERIFY_CHALLENGE', {}).get('success', False)
-    return success
+def resolve_captcha(url, api, driver):
+    try:
+        driver.get(url)
+        WebDriverWait(driver, 86400).until(EC.text_to_be_present_in_element_value((By.NAME, "g-recaptcha-response"), ""))
+        driver.switch_to.frame(driver.find_element_by_xpath("//*/iframe[@title='recaptcha challenge']"))
+        token = driver.find_element_by_id("recaptcha-token").get_attribute("value")
+        response = api.verify_challenge(token=token)
+        success = response.get('responses', {}).get('VERIFY_CHALLENGE', {}).get('success', False)
+        return success
+    except Exception:
+        return False
 
-queue = Queue()
-queue2 = Queue()
+
+captcha = Queue()
+extra = Queue()
 class QueueManager(SyncManager): pass
-QueueManager.register('captcha_queue', callable=lambda:queue)
-QueueManager.register('extra_queue', callable=lambda:queue2)
+QueueManager.register('captcha_queue', callable=lambda:captcha)
+QueueManager.register('extra_queue', callable=lambda:extra)
 manager = QueueManager(address='queue.sock', authkey=b'monkeys')
 manager.connect()
 captcha_queue = manager.captcha_queue()
 extra_queue = manager.extra_queue()
 
+middle_lat = (MAP_START[0] + MAP_END[0]) / 2
+middle_lon = (MAP_START[1] + MAP_END[1]) / 2
+middle_alt = get_altitude((middle_lat, middle_lon))
+
+driver = webdriver.Chrome()
+driver.set_window_size(803, 807)
+
 while not captcha_queue.empty():
+    lat = uniform(middle_lat - 0.001, middle_lat + 0.001)
+    lon = uniform(middle_lon - 0.001, middle_lon + 0.001)
+    alt = uniform(middle_alt - 10, middle_alt + 10)
     account = captcha_queue.get()
-    lat = uniform(40.7473, 40.7479)
-    lon = uniform(-111.8672, -111.8666)
-    alt = get_altitude((lat, lon))
-    device_info = get_device_info(account)
-    api = PGoApi(device_info=device_info)
-    api.set_position(lat, lon, alt)
-    api.set_authentication(username=account[0], password=account[1], provider=account[2])
-    response_dict = api.check_challenge()
-    challenge_url = response_dict.get('responses', {}).get('CHECK_CHALLENGE', {}).get('challenge_url', ' ')
-    if challenge_url != ' ':
-        if resolve_captcha(challenge_url, api):
-            extra_queue.put(account)
-        else:
-            captcha_queue.put(account)
+
+    try:
+        device_info = get_device_info(account)
+        api = PGoApi(device_info=device_info)
+        api.set_position(lat, lon, alt)
+        api.set_authentication(username=account[0], password=account[1], provider=account[2])
+        response_dict = api.check_challenge()
+        challenge_url = response_dict.get('responses', {}).get('CHECK_CHALLENGE', {}).get('challenge_url', ' ')
+        if challenge_url != ' ':
+            if resolve_captcha(challenge_url, api, driver):
+                extra_queue.put(account)
+            else:
+                captcha_queue.put(account)
+    except KeyboardInterrupt:
+        captcha_queue.put(account)
+        driver.close()
+        exit(0)
+    except Exception:
+        captcha_queue.put(account)
+
+driver.close()
