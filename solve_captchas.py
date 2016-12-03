e340542597ee5c96959fb0d43295b1c6bf03dec6
David Christenson <david@the-david.com>
2016-12-03 03:47:37 -0700

    Update solve_captchas
    Use new manager settings and use auth info from accounts dict whenever
    possible.

--- solve_captchas.py
+++ solve_captchas.py
@@ -2,15 +2,17 @@
 
 import pickle
 from queue import Queue
-from multiprocessing.managers import SyncManager
+from multiprocessing.managers import BaseManager
 from pgoapi import (
     exceptions as pgoapi_exceptions,
     PGoApi,
     utilities as pgoapi_utils,
 )
+from pgoapi.auth_ptc import AuthPtc
 from random import uniform
 from utils import random_altitude, get_device_info
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -20,36 +22,32 @@ from sys import exit
 DOWNLOAD_HASH = "5296b4d9541938be20b1d1a8e8e3988b7ae2e93b"
 
 def resolve_captcha(url, api, driver, timestamp):
-    try:
-        driver.get(url)
-        WebDriverWait(driver, 86400).until(EC.text_to_be_present_in_element_value((By.NAME, "g-recaptcha-response"), ""))
-        driver.switch_to.frame(driver.find_element_by_xpath("//*/iframe[@title='recaptcha challenge']"))
-        token = driver.find_element_by_id("recaptcha-token").get_attribute("value")
-        request = api.create_request()
-        request.verify_challenge(token=token)
-        request.check_challenge()
-        request.get_hatched_eggs()
-        request.get_inventory(last_timestamp_ms = timestamp)
-        request.check_awarded_badges()
-        request.download_settings(hash=DOWNLOAD_HASH)
-        request.get_buddy_walked()
+    driver.get(url)
+    WebDriverWait(driver, 86400).until(EC.text_to_be_present_in_element_value((By.NAME, "g-recaptcha-response"), ""))
+    driver.switch_to.frame(driver.find_element_by_xpath("//*/iframe[@title='recaptcha challenge']"))
+    token = driver.find_element_by_id("recaptcha-token").get_attribute("value")
+    request = api.create_request()
+    request.verify_challenge(token=token)
+    request.check_challenge()
+    request.get_hatched_eggs()
+    request.get_inventory(last_timestamp_ms = timestamp)
+    request.check_awarded_badges()
+    request.download_settings(hash=DOWNLOAD_HASH)
+    request.get_buddy_walked()
 
-        response = request.call()
-        success = response.get('responses', {}).get('VERIFY_CHALLENGE', {}).get('success', False)
-        return success
-    except Exception as e:
-        print(e)
-        return False
+    response = request.call()
+    success = response.get('responses', {}).get('VERIFY_CHALLENGE', {}).get('success', False)
+    return success
 
 with open('accounts.pickle', 'rb') as f:
     ACCOUNTS = pickle.load(f)
 
-captcha = Queue()
-extra = Queue()
-class QueueManager(SyncManager): pass
-QueueManager.register('captcha_queue', callable=lambda:captcha)
-QueueManager.register('extra_queue', callable=lambda:extra)
-manager = QueueManager(address='queue.sock', authkey=b'monkeys')
+captcha_queue = Queue()
+extra_queue = Queue()
+class AccountManager(BaseManager): pass
+AccountManager.register('captcha_queue', callable=lambda:captcha_queue)
+AccountManager.register('extra_queue', callable=lambda:extra_queue)
+manager = AccountManager(address='queue.sock', authkey=b'monkeys')
 manager.connect()
 captcha_queue = manager.captcha_queue()
 extra_queue = manager.extra_queue()
@@ -75,15 +73,28 @@ while not captcha_queue.empty():
         lon = uniform(middle_lon - 0.001, middle_lon + 0.001)
         alt = uniform(middle_alt - 10, middle_alt + 10)
 
-    print(username)
-
     try:
         device_info = get_device_info(account)
         api = PGoApi(device_info=device_info)
         api.set_position(lat, lon, alt)
-        api.set_authentication(username=username,
-                               password=account.get('password'),
-                               provider=account.get('provider'))
+
+        authenticated = False
+        if account.get('provider') == 'ptc' and account.get('refresh'):
+            api._auth_provider = AuthPtc()
+            api._auth_provider.set_refresh_token(account.get('refresh'))
+            api._auth_provider._access_token = account.get('auth')
+            api._auth_provider._access_token_expiry = account.get('expiry')
+            if api._auth_provider.check_access_token():
+                print(username, 'already authenticated')
+                api._auth_provider._login = True
+                authenticated = True
+
+        if not authenticated:
+            print(username)
+            api.set_authentication(username=username,
+                                   password=account.get('password'),
+                                   provider=account.get('provider'))
+
         request = api.create_request()
         request.download_remote_config_version(platform=1, app_version=4500)
         request.check_challenge()
@@ -109,10 +120,12 @@ while not captcha_queue.empty():
                 captcha_queue.put(username)
     except KeyboardInterrupt:
         captcha_queue.put(username)
-        driver.close()
-        exit(0)
-    except Exception as e:
-        print(e)
+        break
+    except (WebDriverException, AttributeError):
         captcha_queue.put(username)
+        break
+    except Exception as e:
+        captcha_queue.put(username)
+        raise
 
 driver.close()
