3c37691f77413eefa3665fa56f3e97974802ed91
David Christenson <david@the-david.com>
2017-01-25 20:59:49 -0700

    Improve solve_captchas
    Update to use pogo_async, get middle coordinates from utils, move
    check_challenge to bottom of chain on verification to match client,
    better error handling, detect banned accounts and do not put back on
    queue, print better information, remove some unused imports.

--- solve_captchas.py
+++ solve_captchas.py
@@ -1,45 +1,41 @@
 #!/usr/bin/env python3
 
-from queue import Queue
 from multiprocessing.managers import BaseManager
-from pgoapi import (
-    exceptions,
-    PGoApi,
-    utilities as pgoapi_utils,
-)
-from pgoapi.auth_ptc import AuthPtc
+from pogo_async import PGoApi, exceptions as ex
+from pogo_async.auth_ptc import AuthPtc
+from pogo_async.session import Session
+from asyncio import get_event_loop, sleep
 from random import uniform
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from time import sleep, time
+from time import time
 
 import socket
 
 from pokeminer import config
-from pokeminer.utils import random_altitude, get_device_info, get_address
+from pokeminer.utils import random_altitude, get_device_info, get_address, LAT_MEAN, LON_MEAN
 
-def resolve_captcha(url, api, driver, timestamp):
+
+async def solve_captcha(url, api, driver, timestamp):
     driver.get(url)
     WebDriverWait(driver, 86400).until(EC.text_to_be_present_in_element_value((By.NAME, "g-recaptcha-response"), ""))
     driver.switch_to.frame(driver.find_element_by_xpath("//*/iframe[@title='recaptcha challenge']"))
     token = driver.find_element_by_id("recaptcha-token").get_attribute("value")
     request = api.create_request()
     request.verify_challenge(token=token)
-    request.check_challenge()
     request.get_hatched_eggs()
     request.get_inventory(last_timestamp_ms=timestamp)
     request.check_awarded_badges()
     request.get_buddy_walked()
+    request.check_challenge()
 
-    response = request.call()
-    success = response.get('responses', {}).get('VERIFY_CHALLENGE', {}).get('success', False)
-    return success
+    response = await request.call()
+    return response.get('responses', {}).get('VERIFY_CHALLENGE', {}).get('success', False)
 
 
-def main():
+async def main():
     try:
         if hasattr(config, 'AUTHKEY'):
             authkey = config.AUTHKEY
@@ -59,11 +55,6 @@ def main():
         captcha_queue = manager.captcha_queue()
         extra_queue = manager.extra_queue()
 
-
-        middle_lat = (config.MAP_START[0] + config.MAP_END[0]) / 2
-        middle_lon = (config.MAP_START[1] + config.MAP_END[1]) / 2
-        middle_alt = random_altitude()
-
         driver = webdriver.Chrome()
         driver.set_window_size(803, 807)
 
@@ -72,11 +63,16 @@ def main():
             username = account.get('username')
             location = account.get('location')
             if location and location != (0,0,0):
-                lat, lon, alt = location
+                lat = location[0]
+                lon = location[1]
+                try:
+                    alt = location[2]
+                except IndexError:
+                    alt = random_altitude()
             else:
-                lat = uniform(middle_lat - 0.001, middle_lat + 0.001)
-                lon = uniform(middle_lon - 0.001, middle_lon + 0.001)
-                alt = uniform(middle_alt - 10, middle_alt + 10)
+                lat = uniform(LAT_MEAN - 0.001, LAT_MEAN + 0.001)
+                lon = uniform(LON_MEAN - 0.001, LON_MEAN + 0.001)
+                alt = random_altitude()
 
             try:
                 device_info = get_device_info(account)
@@ -92,56 +88,76 @@ def main():
                     api._auth_provider._access_token = account.get('auth')
                     api._auth_provider._access_token_expiry = account.get('expiry')
                     if api._auth_provider.check_access_token():
-                        print(username, 'already authenticated')
                         api._auth_provider._login = True
                         authenticated = True
 
                 if not authenticated:
-                    print(username)
-                    api.set_authentication(username=username,
-                                           password=account.get('password'),
-                                           provider=account.get('provider'))
+                    await api.set_authentication(username=username,
+                                                 password=account['password'],
+                                                 provider=account.get('provider', 'ptc'))
 
                 request = api.create_request()
-                request.download_remote_config_version(platform=1, app_version=5301)
+                request.download_remote_config_version(platform=1, app_version=5302)
                 request.check_challenge()
                 request.get_hatched_eggs()
                 request.get_inventory()
                 request.check_awarded_badges()
                 request.download_settings()
-                response = request.call()
+                response = await request.call()
                 account['time'] = time()
 
-                responses = response.get('responses', {})
-                challenge_url = responses.get('CHECK_CHALLENGE', {}).get('challenge_url', ' ')
+                if response['status_code'] == 3:
+                    print('{} appears to be banned.'.format(username))
+                    continue
+
+                responses = response['responses']
+                challenge_url = responses['CHECK_CHALLENGE']['challenge_url']
                 timestamp = responses.get('GET_INVENTORY', {}).get('inventory_delta', {}).get('new_timestamp_ms')
                 account['location'] = lat, lon, alt
                 account['inventory_timestamp'] = timestamp
                 if challenge_url == ' ':
                     account['captcha'] = False
+                    print('No CAPTCHA was pending on {}.'.format(username))
                     extra_queue.put(account)
                 else:
-                    if resolve_captcha(challenge_url, api, driver, timestamp):
+                    if await solve_captcha(challenge_url, api, driver, timestamp):
                         account['time'] = time()
                         account['captcha'] = False
+                        print('Solved CAPTCHA for {}, putting back in rotation.'.format(username))
                         extra_queue.put(account)
                     else:
                         account['time'] = time()
-                        print('failure')
+                        print('Failed to solve for {}'.format(username))
                         captcha_queue.put(account)
-            except (KeyboardInterrupt, Exception) as e:
-                print(e)
+            except KeyboardInterrupt:
                 captcha_queue.put(account)
                 break
-            except (exceptions.AuthException, exceptions.AuthTokenExpiredException, exceptions.AuthTokenExpiredException):
-                print('Authentication error')
+            except KeyError:
+                print('Unexpected or empty response for {}, putting back on queue.'.format(username))
                 captcha_queue.put(account)
-                sleep(2)
+                try:
+                    print(response)
+                except Exception:
+                    pass
+                await sleep(3)
+            except (ex.AuthException, ex.AuthTokenExpiredException) as e:
+                print('Authentication error on {}: {}'.format(username, e))
+                captcha_queue.put(account)
+                await sleep(3)
+            except ex.PgoapiError as e:
+                print('pgoapi error on {}: {}'.format(username, e))
+                captcha_queue.put(account)
+                await sleep(3)
+            except Exception:
+                captcha_queue.put(account)
+                raise
     finally:
         try:
             driver.close()
+            Session.close()
         except Exception:
             pass
 
 if __name__ == '__main__':
-    main()
+    loop = get_event_loop()
+    loop.run_until_complete(main())
