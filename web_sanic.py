fdb92449f1d661165f8f461d0a6d9e1ce09a2019
David Christenson <mail@noctem.xyz>
2017-04-19 01:57:42 -0600

    web_sanic improvements
    Use a global connection pool, perform queries directly in response
    functions instead of calling others, delete env after use, localize
    more functions in parameters.

--- web_sanic.py
+++ web_sanic.py
@@ -70,56 +70,97 @@ def render_worker_map():
     ))
 
 
-@app.route('/')
+@app.get('/')
 async def fullmap(request, html_map=render_map()):
     return html_map
-
-
-@app.route('/data')
-async def pokemon_data(request):
-    last_id = request.args.get('last_id', 0)
-    return json(await get_pokemarkers_async(last_id))
-
-
-@app.route('/gym_data')
-async def gym_data(request):
-    return json(await get_gyms_async())
-
-
-@app.route('/spawnpoints')
-async def spawn_points(request):
-    return json([dict(x) for x in await get_spawnpoints_async()])
-
-
-@app.route('/pokestops')
-async def get_pokestops(request):
-    return json([dict(x) for x in await get_pokestops_async()])
-
-
-@app.route('/scan_coords')
-async def scan_coords(request):
-    return json(get_scan_coords())
 
 
 if conf.MAP_WORKERS:
     workers = Workers()
 
 
-    @app.route('/workers_data')
+    @app.get('/workers_data')
     async def workers_data(request):
         return json(get_worker_markers(workers))
 
 
-    @app.route('/workers')
+    @app.get('/workers')
     async def workers_map(request, html_map=render_worker_map()):
         return html_map
 
 
-def sighting_to_marker(pokemon, names=POKEMON, moves=MOVES, damage=DAMAGE):
+del env
+
+
+@app.get('/data')
+async def pokemon_data(request, _time=time):
+    last_id = request.args.get('last_id', 0)
+    async with app.pool.acquire() as conn:
+        results = await conn.fetch('''
+            SELECT id, pokemon_id, expire_timestamp, lat, lon, atk_iv, def_iv, sta_iv, move_1, move_2
+            FROM sightings
+            WHERE expire_timestamp > {} AND id > {}
+        '''.format(_time(), last_id))
+    return json(list(map(sighting_to_marker, results)))
+
+
+@app.get('/gym_data')
+async def gym_data(request, names=POKEMON, _str=str):
+    async with app.pool.acquire() as conn:
+        results = await conn.fetch('''
+            SELECT
+                fs.fort_id,
+                fs.id,
+                fs.team,
+                fs.prestige,
+                fs.guard_pokemon_id,
+                fs.last_modified,
+                f.lat,
+                f.lon
+            FROM fort_sightings fs
+            JOIN forts f ON f.id=fs.fort_id
+            WHERE (fs.fort_id, fs.last_modified) IN (
+                SELECT fort_id, MAX(last_modified)
+                FROM fort_sightings
+                GROUP BY fort_id
+            )
+        ''')
+    return json([{
+            'id': 'fort-' + _str(fort['fort_id']),
+            'sighting_id': fort['id'],
+            'prestige': fort['prestige'],
+            'pokemon_id': fort['guard_pokemon_id'],
+            'pokemon_name': names[fort['guard_pokemon_id']],
+            'team': fort['team'],
+            'lat': fort['lat'],
+            'lon': fort['lon']
+    } for fort in results])
+
+
+@app.get('/spawnpoints')
+async def spawn_points(request, _dict=dict):
+    async with app.pool.acquire() as conn:
+         results = await conn.fetch('SELECT spawn_id, despawn_time, lat, lon, duration FROM spawnpoints')
+    return json([_dict(x) for x in results])
+
+
+@app.get('/pokestops')
+async def get_pokestops(request, _dict=dict):
+    async with app.pool.acquire() as conn:
+        results = await conn.fetch('SELECT external_id, lat, lon FROM pokestops')
+    return json([_dict(x) for x in results])
+
+
+@app.get('/scan_coords')
+async def scan_coords(request):
+    return json(get_scan_coords())
+
+
+def sighting_to_marker(pokemon, names=POKEMON, moves=MOVES, damage=DAMAGE, trash=conf.TRASH_IDS, _str=str):
     pokemon_id = pokemon['pokemon_id']
     marker = {
-        'id': 'pokemon-' + str(pokemon['id']),
-        'trash': pokemon_id in conf.TRASH_IDS,
+        'id': 'pokemon-' + _str(pokemon['id']),
+        'trash': pokemon_id in trash,
         'name': names[pokemon_id],
         'pokemon_id': pokemon_id,
         'lat': pokemon['lat'],
@@ -139,69 +180,15 @@ def sighting_to_marker(pokemon, names=POKEMON, moves=MOVES, damage=DAMAGE):
     return marker
 
 
-async def get_pokemarkers_async(after_id):
-    async with create_pool(**conf.DB) as pool:
-        async with pool.acquire() as conn:
-            async with conn.transaction():
-                results = await conn.fetch('''
-                    SELECT id, pokemon_id, expire_timestamp, lat, lon, atk_iv, def_iv, sta_iv, move_1, move_2
-                    FROM sightings
-                    WHERE expire_timestamp > {ts} AND id > {poke_id}
-                '''.format(ts=time(), poke_id=after_id))
-                return tuple(map(sighting_to_marker, results))
-
-
-async def get_gyms_async(names=POKEMON):
-    async with create_pool(**conf.DB) as pool:
-        async with pool.acquire() as conn:
-            async with conn.transaction():
-                results = await conn.fetch('''
-                    SELECT
-                        fs.fort_id,
-                        fs.id,
-                        fs.team,
-                        fs.prestige,
-                        fs.guard_pokemon_id,
-                        fs.last_modified,
-                        f.lat,
-                        f.lon
-                    FROM fort_sightings fs
-                    JOIN forts f ON f.id=fs.fort_id
-                    WHERE (fs.fort_id, fs.last_modified) IN (
-                        SELECT fort_id, MAX(last_modified)
-                        FROM fort_sightings
-                        GROUP BY fort_id
-                    )
-                ''')
-                return [{
-                        'id': 'fort-' + str(fort['fort_id']),
-                        'sighting_id': fort['id'],
-                        'prestige': fort['prestige'],
-                        'pokemon_id': fort['guard_pokemon_id'],
-                        'pokemon_name': names[fort['guard_pokemon_id']],
-                        'team': fort['team'],
-                        'lat': fort['lat'],
-                        'lon': fort['lon']
-                } for fort in results]
-
-
-async def get_spawnpoints_async():
-    async with create_pool(**conf.DB) as pool:
-        async with pool.acquire() as conn:
-            async with conn.transaction():
-                return await conn.fetch('SELECT spawn_id, despawn_time, lat, lon, duration FROM spawnpoints')
-
-
-async def get_pokestops_async():
-    async with create_pool(**conf.DB) as pool:
-        async with pool.acquire() as conn:
-            async with conn.transaction():
-                return await conn.fetch('SELECT external_id, lat, lon FROM pokestops')
+@app.listener('before_server_start')
+async def register_db(app, loop):
+    app.pool = await create_pool(**conf.DB, loop=loop)
 
 
 def main():
     args = get_args()
     app.run(debug=args.debug, host=args.host, port=args.port)
+
 
 if __name__ == '__main__':
     main()
